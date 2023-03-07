ea2f5831517c7b4a2cc55bf4d5a392ca08c615f3
gabinbusssonneau <101792230+dexterwho1@users.noreply.github.com>
2023-03-07 22:27:29 +0100

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -3,286 +3,269 @@ require_once "db.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="homepage.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-tDZBOSXpx2vtyGJPH/BrB/3sNC6UvxQ6UaZ6NDH1+IH3z9g9KjG9fR5C45z+DDh4BcYbivJOfn/x+kvfnBAdFg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="homepage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-tDZBOSXpx2vtyGJPH/BrB/3sNC6UvxQ6UaZ6NDH1+IH3z9g9KjG9fR5C45z+DDh4BcYbivJOfn/x+kvfnBAdFg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-        <title>Netfl-Hess</title>
-    </head>
-    <body>
+    <title>Netfl-Hess</title>
+</head>
+<body>
 
-        <header class="containerHeader">
+<header class="containerHeader">
 
-            <!-- side Bar -->
-            <nav class="sideBarHeader">
-            <i>
-                Accueil
-            </i>
+    <!-- side Bar -->
+    <nav class="sideBarHeader">
+        <i>
+            Accueil
+        </i>
 
-            <i>
-                Film
-            </i>
+        <i>
+            Film
+        </i>
 
-            <i>
-                Série
-            </i>
+        <i>
+            Série
+        </i>
 
-            <i>
-                Auteur
-            </i>
+        <i>
+            Auteur
+        </i>
 
-            <i>
-                Dernière nouveauté
-            </i>
+        <i>
+            Dernière nouveauté
+        </i>
 
-            <i>
-                lancer un film au hasard
-            </i>
+        <i>
+            lancer un film au hasard
+        </i>
 
 
 
-            </nav>
+    </nav>
 
-            <!-- menu horizontal -->
+    <!-- menu horizontal -->
 
-            <nav class="menuHeader">
-                <form>
-                    <input type="search" placeholder="Alice au pays des merveilles">
-                    <input type="submit">
+    <nav class="menuHeader">
+        <form>
+            <input type="search" placeholder="Alice au pays des merveilles">
+            <input type="submit">
 
 
-                </form>
+        </form>
 
-                <div class="userHeader">
-                    <li>
-                        Panier
-                    </li>
+        <div class="userHeader">
+            <li>
+                Panier
+            </li>
 
-                    <li>
-                        utilisateur
-                    </li>
+            <li>
+                utilisateur
+            </li>
 
-                </div>
-            </nav>
+        </div>
+    </nav>
 
-        </header>
+</header>
 
-        <!-- principal -->
+<!-- principal -->
 
-        <section>
+<section>
 
-            <!-- film ou série en vedette -->
+    <!-- film ou série en vedette -->
 
-            <article class="filmvedettesection">
+    <article class="filmvedettesection">
 
-                <?php
-                $req= $bdd->prepare('SELECT titre, synopsis, affiche , realisateur, studio, date_parution from film ORDER BY RAND() LIMIT 1 ');
-                $req->execute();
-                while ($donnee= $req->fetch()){
-                    echo'<div class="titreFilmvedetteSection" style="    background-image: url(image/homepage/';
-                    echo $donnee['affiche'];
-                    echo');">';
-                    echo $donnee['titre'];
-                    echo'</div>';
-                    echo'<div class="resumeFilmvedetteSection">';
-                    echo $donnee['synopsis'];
-                    echo '</div>';
-                    echo'<div class="auteurFilmvedetteSection">';
-                    echo'<p> de ';
+        <?php
+        $req = $bdd->prepare('SELECT titre, synopsis, affiche, realisateur, studio, date_parution, prix FROM film ORDER BY RAND() LIMIT 1');
+        $req->execute();
+        while ($donnee = $req->fetch()) {
+            echo '<style>.filmvedettesection{background-image: url(image/homepage/affiche/'. $donnee['affiche'] .');}</style>';
+            echo '<div class="titreFilmvedetteSection">';
+            echo $donnee['titre'];
+            echo '</div>';
+            echo '<div class="resumeFilmvedetteSection">';
+            echo $donnee['synopsis'];
+            echo '</div>';
+            echo '<div class="auteurFilmvedetteSection">';
+            echo '<p> de ' . $donnee['realisateur'] . ' - ' . $donnee['date_parution'] . ' - ' . $donnee['studio'] . '</p>';
+            echo '</div>';
 
-                    echo $donnee['realisateur'];
-                    echo $donnee['date_parution'];
-                    echo $donnee['studio'];
-                    echo '</p>';
+            echo '<div class="genreFilmvedetteSection">';
+            echo '<ul>';
+            echo '<li>Décalé</li>';
+            echo '<li>Sombre</li>';
+            echo '<li>Humour noir</li>';
+            echo '</ul>';
+            echo '</div>';
 
-                }
+            echo '<div class="buyFilmvedetteSection">';
+            echo '<p class="pbuyFilmvedetteSection">' . $donnee['prix'] . '</p>';
+            echo '<button>Mettre au panier</button>';
+            echo '</div>';
+        }
+        ?>
 
-                ?>
+    </article>
 
-                <div class="genreFilmvedetteSection">
-                    <li>
-                        Décalé
-                    </li>
+    <!-- liste d'émoticones servant à trier les films ou séries  par genre -->
 
-                    <li>
-                        Sombre
-                    </li>
+    <nav class="navCategory">
+        <?php
+        $req = $bdd->prepare('SELECT nom, image FROM genre');
+        $req->execute();
 
-                    <li>
-                        humour noir
-                    </li>
-                </div>
+        while ($donnee = $req->fetch()) {
+            echo '<li>';
+            echo '<div class="image-container">';
+            echo '<img style="height: 60px; width: 50px;" class="imageNavCategory" src="image/homepage/category/'.$donnee['image'].'" />';
+            echo '<span>'.$donnee["nom"].'</span>';
+            echo '</div>';
+            echo '</li>';
+        }
+        ?>
 
-                <div class="buyFilmvedetteSection">
-                    <p class="pbuyFilmvedetteSection">
-                        PRIX
-                    </p>
-                    <button>
-                        Mettre au panier
-                    </button>
-                </div>
 
-            </article>
+    </nav>
 
-            <!-- liste d'émoticones servant à trier les films ou séries  par genre -->
+    <!-- film ou séries trié -->
 
-            <nav class="navCategory">
-                <?php
-                $req = $bdd->prepare('SELECT nom, image FROM genre');
-                $req->execute();
 
-                while ($donnee = $req->fetch()) {
-                    echo '<li>';
-                    echo '<div class="image-container">';
-                    echo '<img style="height: 60px; width: 50px;" class="imageNavCategory" src="image/homepage/category/'.$donnee['image'].'" />';
-                    echo '<span>'.$donnee["nom"].'</span>';
-                    echo '</div>';
-                    echo '</li>';
-                }
-                ?>
 
+    <aside class="movieGallery">
 
-            </nav>
+        <?php
+        $req = $bdd->prepare('SELECT realisateur, image, titre, date_parution FROM film');
+        $req->execute();
 
-            <!-- film ou séries trié -->
 
+        while ($donnee = $req->fetch()) {
 
 
-            <aside class="movieGallery">
+            echo '<li class="singlemovieGallery">';
+            echo '<img src="image/homepage/poster/'.$donnee['image'].'" />';
+            echo '<p>'. $donnee['titre'] .' de '. $donnee['realisateur'] .' en '. $donnee['date_parution'] .'</p>';
+            echo '</li>';
+        }
 
-                <?php
-                $req = $bdd->prepare('SELECT realisateur, image, titre, date_parution FROM film');
-                $req->execute();
 
 
-                while ($donnee = $req->fetch()) {
 
+        ?>
 
-                    echo '<li class="singlemovieGallery">';
-                    echo '<img src="image/homepage/poster/'.$donnee['image'].'" />';
-                    echo '<p>'. $donnee['titre'] .' de '. $donnee['realisateur'] .' en '. $donnee['date_parution'] .'</p>';
-                    echo '</li>';
-                }
 
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                ?>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+        <li class="singlemovieGallery">
+            <img src="image/homepage/movieGallery.png">
+            <p> La nuit des fantomes De Steven Gray 1978</p>
+        </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
+    </aside>
 
-                </li>
 
-                <li class="singlemovieGallery">
-                    <img src="image/homepage/movieGallery.png">
-                    <p> La nuit des fantomes De Steven Gray 1978</p>
-                </li>
+</section>
 
 
+<footer>
 
-            </aside>
 
-
-        </section>
-
-
-        <footer>
-
-
-        </footer>
-    </body>
+</footer>
+</body>
 </html>

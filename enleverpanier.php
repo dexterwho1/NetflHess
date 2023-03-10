ccf5a6ec779d07ce0b314631e754c2779e335998
gabinbusssonneau <101792230+dexterwho1@users.noreply.github.com>
2023-03-10 11:07:45 +0100

    Add files via upload

--- enleverpanier.php
+++ enleverpanier.php
@@ -1,11 +1,16 @@
 <?php
 require_once "db.php";
 session_start();
-echo $_GET['id'];
-$req= $bdd->prepare('delete  from film join panier on film.id=panier.id_film where id_utilisateur=:id');
-$req->execute(array(
-    ':id_utilisateur' =>$_SESSION['email']
-));
 
-header('location:panier.php');
+if (isset($_GET['id'])) {
+    $req = $bdd->prepare('DELETE FROM panier WHERE id_utilisateur = :id_utilisateur AND id_film = :id_film');
+    $req->execute(array(
+        ':id_utilisateur' => $_SESSION['email'],
+        ':id_film' => $_GET['id']
+    ));
+    $req->closeCursor();
+}
+
+header('location: panier.php');
+exit();
 ?>
